f2762fa51cbb435a89b36c8bec6aaf7c336d42f7
web-collab <tanmoy587@gmail.com>
2025-02-26 20:40:31 +0530

    Forntend is completed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,18 +5,18 @@ const Footer = () => {
     <footer>
       <div className="container">
         <div className="banner">
-          <div className="left">ZEESHU</div>
+          <div className="left"></div>
           <div className="right">
-            <p>Z6 Gulshan-e-Maymar, Karachi</p>
-            <p>Open: 05:00 PM - 12:00 AM</p>
+            <p>RDB Boulevard, GP Block, Sector V, Bidhannagar, Kolkata, West Bengal</p>
+            <p>Open: 12:00 PM - 12:00 AM</p>
           </div>
         </div>
         <div className="banner">
           <div className="left">
-            <p>Developed By CODEWITHZEESHU</p>
+            <p>Developed By Tanmay Samanta</p>
           </div>
           <div className="right">
-            <p>All Rights Reserved By CodeWithZeeshu.</p>
+            <p>All Rights Reserved By TanmaySamanta.</p>
           </div>
         </div>
       </div>
