d1f7888cd1b1df529a9c0bc7ed6ab78c496197ef
web-collab <tanmoy587@gmail.com>
2025-02-28 10:26:23 +0530

    FRONTEND Some modification

--- frontend/src/components/Team.jsx
+++ frontend/src/components/Team.jsx
@@ -7,9 +7,7 @@ const Team = () => {
         <div className="heading_section">
           <h1 className="heading">OUR TEAM</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-            fugit dicta, ipsum impedit quam laboriosam quas doloremque quia
-            perferendis laborum.
+Meet our passionate team of culinary experts and hospitality professionals, dedicated to delivering exceptional dining experiences with every meal.
           </p>
         </div>
         <div className="team_container">
