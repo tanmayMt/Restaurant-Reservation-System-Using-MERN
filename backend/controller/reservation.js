fe458ceb2575174937af114837c69656c175a0ef
web-collab <tanmoy587@gmail.com>
2025-03-16 18:12:30 +0530

    no

--- backend/controller/reservation.js
+++ backend/controller/reservation.js
@@ -1,6 +1,5 @@
 import ErrorHandler from "../middlewares/error.js";
 import { Reservation } from "../models/reservation.js";
-import nodemailer from "nodemailer";
 
 
 const send_reservation = async (req, res, next) => {
